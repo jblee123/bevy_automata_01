56f8b19c705b78f938503e85bd89b12cff2dcc6e
Brian Lee <jblee123@gmail.com>
2023-12-03 15:44:48 -0500

    Life rules working with "Acorn" seed.

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,20 @@ const CELL_COLOR: Color = Color::BLUE;
 
 const BASE_CELL_SIZE: f32 = 20.;
 
+const BIRTH_RULES: [bool; 9] = [false, false, false, true, false, false, false, false, false];
+const SURVIVAL_RULES: [bool; 9] = [false, false, true, true, false, false, false, false, false];
+
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 struct CellCoord {
     pub x: i32,
     pub y: i32,
+}
+
+impl CellCoord {
+    const MIN_X: i32 = i32::MIN;
+    const MAX_X: i32 = i32::MAX;
+    const MIN_Y: i32 = i32::MIN;
+    const MAX_Y: i32 = i32::MAX;
 }
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
@@ -31,16 +41,29 @@ impl Cell {
 struct Colony {
     pub cells: Vec<Cell>,
     pub new_cells: Vec<Cell>,
+    pub neighbor_coords: Vec<CellCoord>,
+}
+
+impl Colony {
+    pub fn new() -> Self {
+        Self {
+            cells: vec![],
+            new_cells: vec![],
+            neighbor_coords: vec![],
+        }
+    }
 }
 
 #[derive(Component)]
 struct SimState {
+    pub is_paused: bool,
     pub last_generation_time_sec: f64,
 }
 
 impl SimState {
     pub fn new() -> Self {
         Self {
+            is_paused: true,
             last_generation_time_sec: 0.,
         }
     }
@@ -68,19 +91,24 @@ fn setup(mut commands: Commands) {
 
     commands.spawn(SimState::new());
 
-    commands.spawn(Colony {
-        cells: vec![
-            Cell::new(CellCoord { x: 0, y: 0 }),
-            Cell::new(CellCoord { x: 1, y: 0 }),
-            Cell::new(CellCoord { x: 3, y: 1 }),
-        ],
-        new_cells: vec![],
-    });
+    let mut colony = Colony::new();
+    colony.cells = vec![
+        Cell::new(CellCoord { x: 0, y: 0 }),
+        Cell::new(CellCoord { x: 1, y: 0 }),
+        Cell::new(CellCoord { x: 4, y: 0 }),
+        Cell::new(CellCoord { x: 5, y: 0 }),
+        Cell::new(CellCoord { x: 6, y: 0 }),
+        Cell::new(CellCoord { x: 3, y: 1 }),
+        Cell::new(CellCoord { x: 1, y: 2 }),
+    ];
+    colony.cells.sort();
+    commands.spawn(colony);
 }
 
 fn update_colony(
     mut colony_query: Query<&mut Colony>,
     mut sim_state_query: Query<&mut SimState>,
+    keys: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
     let colony = colony_query.iter_mut().next();
@@ -94,36 +122,23 @@ fn update_colony(
     }
 
     let mut sim_state = sim_state.unwrap();
-    let next_gen_time = sim_state.last_generation_time_sec + GENERATION_PERIOD_SEC;
-    let now_sec = time.elapsed_seconds_f64();
-    if now_sec < next_gen_time {
-        return;
+
+    if keys.just_pressed(KeyCode::Space) {
+        sim_state.is_paused = !sim_state.is_paused;
     }
 
-    sim_state.last_generation_time_sec = now_sec;
+    let step_next = keys.just_pressed(KeyCode::N);
+
+    let next_gen_time = sim_state.last_generation_time_sec + GENERATION_PERIOD_SEC;
+    let now_sec = time.elapsed_seconds_f64();
+    let run_step = step_next || (now_sec >= next_gen_time && !sim_state.is_paused);
 
     let mut colony = colony.unwrap();
 
-    // Pull out cells and new_cells so we're not working with them in the
-    // context of the whole colony struct, which means easier borrow checking.
-    let mut cells = Vec::<Cell>::new();
-    std::mem::swap(&mut colony.cells, &mut cells);
-
-    let mut new_cells = Vec::<Cell>::new();
-    std::mem::swap(&mut colony.new_cells, &mut new_cells);
-
-    new_cells.clear();
-
-    let living_cells = cells.iter().filter(|cell| cell.alive);
-    for cell in living_cells {
-        new_cells.push(*cell);
-        new_cells.last_mut().unwrap().coord.x *= -1;
+    if run_step {
+        run_next_generation(&mut colony);
+        sim_state.last_generation_time_sec = now_sec;
     }
-
-    // Replace our working vectors back into the colony, but switch cells and
-    // new_cells.
-    std::mem::swap(&mut colony.cells, &mut new_cells);
-    std::mem::swap(&mut colony.new_cells, &mut cells);
 }
 
 fn update_display(
@@ -154,4 +169,120 @@ fn update_display(
             });
         }
     }
+}
+
+fn run_next_generation(colony: &mut Colony) {
+    // Pull out cells and new_cells so we're not working with them in the
+    // context of the whole colony struct, which means easier borrow checking.
+    let mut cells = std::mem::take(&mut colony.cells);
+    let mut new_cells = std::mem::take(&mut colony.new_cells);
+    let mut neighbor_coords = std::mem::take(&mut colony.neighbor_coords);
+
+    new_cells.clear();
+    new_cells.reserve(cells.len() * 2);
+
+    const NEIGHBORS_PER_CELL: usize = 8;
+    neighbor_coords.clear();
+    neighbor_coords.reserve(cells.len() * NEIGHBORS_PER_CELL);
+
+    for cell in &cells {
+        if !cell.alive {
+            continue;
+        }
+
+        let is_not_min_x = cell.coord.x != CellCoord::MIN_X;
+        let is_not_max_x = cell.coord.x != CellCoord::MAX_X;
+        let is_not_min_y = cell.coord.y != CellCoord::MIN_Y;
+        let is_not_max_y = cell.coord.y != CellCoord::MAX_Y;
+
+        if is_not_min_x && is_not_min_y {
+            neighbor_coords.push(CellCoord {
+                x: cell.coord.x - 1,
+                y: cell.coord.y - 1,
+            });
+        }
+        if is_not_min_x {
+            neighbor_coords.push(CellCoord {
+                x: cell.coord.x - 1,
+                y: cell.coord.y,
+            });
+        }
+        if is_not_min_x && is_not_max_y {
+            neighbor_coords.push(CellCoord {
+                x: cell.coord.x - 1,
+                y: cell.coord.y + 1,
+            });
+        }
+
+        if is_not_min_y {
+            neighbor_coords.push(CellCoord {
+                x: cell.coord.x,
+                y: cell.coord.y - 1,
+            });
+        }
+        if is_not_max_y {
+            neighbor_coords.push(CellCoord {
+                x: cell.coord.x,
+                y: cell.coord.y + 1,
+            });
+        }
+
+        if is_not_max_x && is_not_min_y {
+            neighbor_coords.push(CellCoord {
+                x: cell.coord.x + 1,
+                y: cell.coord.y - 1,
+            });
+        }
+        if is_not_max_x {
+            neighbor_coords.push(CellCoord {
+                x: cell.coord.x + 1,
+                y: cell.coord.y,
+            });
+        }
+        if is_not_max_x && is_not_max_y {
+            neighbor_coords.push(CellCoord {
+                x: cell.coord.x + 1,
+                y: cell.coord.y + 1,
+            });
+        }
+    }
+
+    neighbor_coords.sort();
+
+    let mut cell_idx = 0;
+    let mut neighbor_idx2 = 0;
+    while neighbor_idx2 < neighbor_coords.len() {
+        // Skip elements that are the same in neighbors list.
+        let neighbor_idx1 = neighbor_idx2;
+        neighbor_idx2 = neighbor_idx2 + 1;
+        while neighbor_idx2 < neighbor_coords.len()
+            && (neighbor_coords[neighbor_idx1] == neighbor_coords[neighbor_idx2])
+        {
+            neighbor_idx2 = neighbor_idx2 + 1;
+        }
+
+        // Find the first element in the cells >= the current element in the neighbors
+        while ((neighbor_coords[neighbor_idx1] > cells[cell_idx].coord) || !cells[cell_idx].alive)
+            && ((cell_idx + 1) < cells.len())
+        {
+            cell_idx = cell_idx + 1;
+        }
+
+        let num_neighbors = neighbor_idx2 - neighbor_idx1;
+        let was_alive = neighbor_coords[neighbor_idx1] == cells[cell_idx].coord;
+        let is_alive = if was_alive {
+            SURVIVAL_RULES[num_neighbors]
+        } else {
+            BIRTH_RULES[num_neighbors]
+        };
+        if is_alive {
+            new_cells.push(Cell::new(neighbor_coords[neighbor_idx1]));
+        }
+    }
+
+    // Replace our working vectors back into the colony, but switch cells and
+    // new_cells.
+    std::mem::swap(&mut colony.cells, &mut new_cells);
+    std::mem::swap(&mut colony.new_cells, &mut cells);
+    std::mem::swap(&mut colony.neighbor_coords, &mut neighbor_coords);
 }
